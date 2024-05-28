99e9b2c3ee50d49cfe8477001496430802d5fab2
apotofkoolaid <72956636+apotofkoolaid@users.noreply.github.com>
2024-05-27 21:05:32 -0500

    Interlude box recipes

--- Items/Placeables/Interlude1MusicBox.cs
+++ Items/Placeables/Interlude1MusicBox.cs
@@ -1,3 +1,4 @@
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace CalamityModMusic.Items.Placeables
@@ -5,5 +6,22 @@ namespace CalamityModMusic.Items.Placeables
     public class Interlude1MusicBox : MusicBox
     {
         public override int MusicBoxTile => ModContent.TileType<Tiles.Interlude1MusicBox>();
+
+        public override void AddRecipes()
+        {
+            // Interludes only play once per world (when toggled, otherwise it doesn't play in-game)
+            CreateRecipe().
+                AddIngredient(ModContent.ItemType<DesertScourgeMusicBox>()).
+                AddIngredient(ModContent.ItemType<CrabulonMusicBox>()).
+                AddIngredient(ModContent.ItemType<HiveMindMusicBox>()).
+                AddIngredient(ModContent.ItemType<PerforatorsMusicBox>()).
+                AddIngredient(ModContent.ItemType<SlimeGodMusicBox>()).
+                AddIngredient(ModContent.ItemType<CryogenMusicBox>()).
+                AddIngredient(ModContent.ItemType<AquaticScourgeMusicBox>()).
+                AddIngredient(ModContent.ItemType<BrimstoneElementalMusicBox>()).
+                AddIngredient(ModContent.ItemType<CalamitasCloneMusicBox>()).
+                AddTile(TileID.TinkerersWorkbench).
+                Register();
+        }
     }
 }
